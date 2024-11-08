c3a156c2852c7841e230531761d95ab24eedbfae
VajjhaRajeshKumar <vajjharajeshkumar@gmail.com>
2024-11-08 13:25:02 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,12 @@
+class Emp
+{
+public static void main(String args[])
+}
+int eno=101;
+string ename="Karun";
+float esal=544.45F;
+  System.out.println("the given employee is number:"+eno);
+  System.out.println("the given employee is name:"+ename);
+  System.out.println("the given employee salary:"+esal);
+}
+}                                                                                                      
---
+++ First.java
@@ -0,0 +1,16 @@
+
+//sample program
+/*sample
+  java
+  program
+*/
+import java.lang.System;
+import java.lang.String;
+// import java.lang.*;
+class First
+{
+ public static void main(String args[])
+ {
+ System.out.println("THIS IS MY FIRST PROGRAM");
+}
+}
